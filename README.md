0ef60afde5a139cb7f12b7c686737db4aa15f60f
mohammadhaimour <mohammadhaimour1999@gmail.com>
2022-06-26 01:01:52 +0300

    class11

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # airline-system-
+
+Builded control system for an Airline where it will keep tracking each flight status by contacting the pilot of that flight who in turn will inform the manager and the system when a flight took-off and arrived.
+
+![](./uml11.PNG)
---
+++ src/pilot/pilot.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const events = require('../events');
+const { faker } = require('@faker-js/faker');
+require('../system/system');
+
+let ID = faker.datatype.uuid();
+
+events.on('new-flight', pilot);
+
+function pilot() {
+    setTimeout(() => {
+        let tookOff = `Pilot: flight with ID ${ID} took-off`;
+        console.log(tookOff);
+        events.emit('took-off', tookOff);
+    }, 4000);
+
+    setTimeout(() => {
+        let arrived = `Pilot: flight with ID ${ID} has arrived`;
+        console.log(arrived);
+        events.emit('Arrived', arrived);
+    }, 7000);
+
+}
